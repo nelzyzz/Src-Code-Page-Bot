05147b731437c3ac6c6a9d122f8fb2d105a2f6a0
nelzyzz <sunnel893@gmail.com>
2024-11-01 21:23:09 +0800

    Update handlePostback.js

--- handles/handlePostback.js
+++ handles/handlePostback.js
@@ -7,7 +7,7 @@ const handlePostback = async (event, pageAccessToken) => {
   if (!senderId || !payload) return console.error('Invalid postback event object');
 
   try {
-    await sendMessage(senderId, { text: ``Hello, I'm Cleo AI DEVELOPED BY SUNNEL JOHN REBANO,\n\n your friendly assistant here to help with any question or any task you have! (⁠◍⁠•⁠ᴗ⁠•⁠◍⁠) \n\nHow can I assist you?` }, pageAccessToken);
+    await sendMessage(senderId, { text: `Hello, I'm Cleo AI DEVELOPED BY SUNNEL JOHN REBANO,\n\n your friendly assistant here to help with any question or any task you have! (⁠◍⁠•⁠ᴗ⁠•⁠◍⁠) \n\nHow can I assist you?` }, pageAccessToken);
   } catch (err) {
     console.error('Error sending postback response:', err.message || err);
   }
